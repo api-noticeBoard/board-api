6a185f9c3deeb58619c491ae0f2210e05e8a60dd
jimiboy <jimi008285@gmail.com>
2025-08-20 11:45:35 +0900

    new project

---
+++ src/main/java/com/portfolio/board/api/domain/Post.java
@@ -0,0 +1,12 @@
+package com.portfolio.board.api.domain;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Post {
+}
